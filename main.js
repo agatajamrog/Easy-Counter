2fb275ede3270a2c35e9d24dddc0e11da3fde999
Agata Jamróg <71328095+agatajamrog@users.noreply.github.com>
2021-01-11 11:26:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const buttonClick = document.querySelector("button");
+let number = 1;
+
+const addElement = function () {
+    const div = document.createElement('div');
+    div.textContent = number;
+    if (number%5 == 0) {
+        div.classList.add('circle')
+    }
+    document.body.appendChild(div);
+    number++;
+} 
+buttonClick.addEventListener("click", addElement)
+   
